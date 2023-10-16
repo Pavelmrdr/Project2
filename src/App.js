83fd6cc5e775b885262e70b128ab6c0f25a2b54d
Enni Mac <pavelmardari@Pavels-MacBook-Pro.local>
2023-10-16 21:15:14 +0300

    inproved styling and code structure

--- src/App.js
+++ src/App.js
@@ -6,34 +6,36 @@ import SebastienCronImg from "./images/1652378194247-fotor-20230905162913.png";
 import cristianBeresImg from "./images/1579200632404.jpeg";
 
 import "./index.css";
+import Header from "./components/Header";
+
+const content = [
+  {
+    image: joSpoltonImg,
+    name: "Jo Spolton",
+    position: "Founder, Rumage",
+    dsc: "7code delivered the project on time, and the client experienced partner growth, onboarding metrics, and retention rates. They communicated frequently and promptly via email and virtual meetings, ensuring an effective workflow. Their understanding of the client's system was a hallmark of their work.",
+  },
+  {
+    image: paulEganImg,
+    name: "Paul Egan",
+    position: "CTO, Founders Factory",
+    dsc: "All module deadlines have been met thanks to 7code’s consistent communication and technical competence. Their code’s quality is exceptionally noteworthy.",
+  },
+  {
+    image: SebastienCronImg,
+    name: "Sebastien Cron",
+    position: "Project Manager, Numerize",
+    dsc: "We worked on the responsive part of our GED (Electronic Document Manager) tool with 7code. The project management was perfect from start to finish. All the work was carried out correctly, on time, and fully transparent.",
+  },
+  {
+    image: cristianBeresImg,
+    name: "Cristian Beres",
+    position: "CEO, Synergo Group",
+    dsc: "The 7Code team has been a great partner to the Synergo Group. They are knowledgeable, creative and they adapt easily. Highly recommended",
+  },
+];
 
 const App = () => {
-  const content = [
-    {
-      image: joSpoltonImg,
-      name: "Jo Spolton",
-      position: "Founder, Rumage",
-      dsc: "7code delivered the project on time, and the client experienced partner growth, onboarding metrics, and retention rates. They communicated frequently and promptly via email and virtual meetings, ensuring an effective workflow. Their understanding of the client's system was a hallmark of their work.",
-    },
-    {
-      image: paulEganImg,
-      name: "Paul Egan",
-      position: "CTO, Founders Factory",
-      dsc: "All module deadlines have been met thanks to 7code’s consistent communication and technical competence. Their code’s quality is exceptionally noteworthy.",
-    },
-    {
-      image: SebastienCronImg,
-      name: "Sebastien Cron",
-      position: "Project Manager, Numerize",
-      dsc: "We worked on the responsive part of our GED (Electronic Document Manager) tool with 7code. The project management was perfect from start to finish. All the work was carried out correctly, on time, and fully transparent.",
-    },
-    {
-      image: cristianBeresImg,
-      name: "Cristian Beres",
-      position: "CEO, Synergo Group",
-      dsc: "The 7Code team has been a great partner to the Synergo Group. They are knowledgeable, creative and they adapt easily. Highly recommended",
-    },
-  ];
 
   let [indexNumber, setIndexNumber] = useState(0);
   
@@ -63,7 +65,7 @@ const App = () => {
 
   return (
     <div className="container">
-        
+        <Header />
       <article>
         <img src={content[indexNumber].image} alt={content[indexNumber].name} />
         <h3>{content[indexNumber].name}</h3>
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+const Header = () => {
+  return (
+    <div>
+      <h1>Our Reviews</h1>
+      <div className="line"></div>
+    </div>
+  );
+};
+
+export default Header
